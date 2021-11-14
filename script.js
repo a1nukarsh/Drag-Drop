a148e7c703b75d6d2d9307d61fd4afbb9be0880a
Anukarsh <anukarshr@gmail.com>
2021-11-14 15:38:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log("D&D")
+console.log("D&D");
 
-const imgBox = document.querySelector('.imgBox')
-const whiteBoxes = document.getElementsByClassName('whiteBox')
+const imgBox = document.querySelector(".imgBox");
+const whiteBoxes = document.getElementsByClassName("whiteBox");
+
+imgBox.addEventListener("dragstart", () => {
+  console.log("Drag start has been triggered");
+});
+imgBox.addEventListener("dragend", () => {
+  console.log("Drag end has been triggered");
+});
