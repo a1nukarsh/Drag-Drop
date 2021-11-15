2ff02f5e4c3c6af60f868d18ff685e4fd05bf1b4
Anukarsh <anukarshr@gmail.com>
2021-11-15 23:20:20 +0530

    update

--- script.js
+++ script.js
@@ -5,7 +5,23 @@ const whiteBoxes = document.getElementsByClassName("whiteBox");
 
 imgBox.addEventListener("dragstart", () => {
   console.log("Drag start has been triggered");
+  
 });
 imgBox.addEventListener("dragend", () => {
   console.log("Drag end has been triggered");
 }); 
+
+for(whiteBox of whiteBoxes){
+  whiteBox.addEventListener('dragover',()=>{
+    
+  })
+  whiteBox.addEventListener('dragenter',()=>{
+
+  })
+  whiteBox.addEventListener('dragleave',()=>{
+
+  })
+  whiteBox.addEventListener('drop',()=>{
+
+  })
+}
